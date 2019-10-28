fb75ed7fc9bc38fac06bb1d2437ea7dbc00e6e43
Erin  Rose <errose10@gmail.com>
2019-10-28 14:51:35 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,6 +3,14 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
+board = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
+ 
+top_row_win = [0,1,2]
+ 
+if board[top_row_win[0]] == "X" && board[top_row_win[1]] == "X" && board[top_row_win[2]] == "X"
+  "X won in the top row"
+end
+
 WIN_COMBINATIONS = [
   [0,1,2], # Top row
   [3,4,5],  # Middle row
