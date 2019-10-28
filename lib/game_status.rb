d58265605f976873cd43848e9dfd831f77cd22d1
Erin  Rose <errose10@gmail.com>
2019-10-28 14:51:38 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,9 +4,9 @@ def position_taken?(board, index)
 end
 
 board = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
- 
+
 top_row_win = [0,1,2]
- 
+
 if board[top_row_win[0]] == "X" && board[top_row_win[1]] == "X" && board[top_row_win[2]] == "X"
   "X won in the top row"
 end
